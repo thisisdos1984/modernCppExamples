0b2199aa3b359ef850b79453f315f5eb244aad0f
Dos <thisisdos1984@yahoo.com>
2017-11-29 13:28:51 -0500

    add few comments

--- algorithm/dp/longestPalindrom.cpp
+++ algorithm/dp/longestPalindrom.cpp
@@ -2,6 +2,14 @@
 #include <string>
 #include <vector>
 
+/* Program to find longest palindrome
+ * using dynamic programming. The idea is to use optimal subsolution 
+ * recursively to get the final output. 
+ */
+
+
+// print the answer table
+
 void PrintTable(const std::vector<std::vector<bool>> & table)
 {
 	for (auto x : table)
@@ -11,13 +19,14 @@ void PrintTable(const std::vector<std::vector<bool>> & table)
 		std::cout << std::endl;
 	}
 }
+// prints all the palindromes except single character palindromes
 void PrintAllPalindromes(const std::string &input, const std::vector<std::vector<bool>> &table)
 {
 	for(size_t i = 0; i < table.size(); i++)
 	{
 		for (size_t j = 0; j < table[i].size(); j++)
 		{
-			if (i != j && table[i][j] == true)
+			if (i != j && table[i][j] == true) // is palindrome at i and j except single character 
 			{
 				std::cout << std::string (input.begin()+i, input.begin()+j+1) << std::endl;
 			}
@@ -25,17 +34,22 @@ void PrintAllPalindromes(const std::string &input, const std::vector<std::vector
 	}
 }
 
-
+//find longest palindrome substring using dynamic programming
 std::string LongestPalindromeSubstring(const std::string & input)
 {
+	//[n][n] matrix of booleans
 	std::vector <std::vector <bool>> table (input.size(), std::vector<bool>(input.size(), false));
 
 	int max_i = 0, max_j = 0;
-
+	// all diagonal elements are palindromes
 	for (size_t i = 0; i < input.size(); i++)
 	{
 		table[i][i] = true;
 	}
+
+	//build the table using previous answers 
+	//table[i][j] is true if substring in 
+	//between i and j is also a palindrome
 
 	for (size_t l = 1; l < input.size(); l++)
 	{
@@ -67,7 +81,6 @@ std::string LongestPalindromeSubstring(const std::string & input)
 	PrintAllPalindromes(input, table);
 
 	std::string output (input.begin() + max_i, input.begin() + max_j + 1);
-
 	return output;
 }
 
@@ -82,3 +95,4 @@ int main()
 	LongestPalindromeSubstring("bananas");
 	return 0;
 }
+
