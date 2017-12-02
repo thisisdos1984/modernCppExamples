43ef3cdcfd0e73f4d5d2fc5c96be792a0b01468b
Dos <thisisdos1984@yahoo.com>
2017-12-01 22:55:08 -0500

    Implement rule of 5

--- dataStructures/QUsingStacks.cpp
+++ dataStructures/QUsingStacks.cpp
@@ -18,13 +18,55 @@ class Queue
 
    public:
 	Queue() : active(new std::stack<int>()), passive(new std::stack<int>()) {}
+	Queue(const Queue & other);
+	Queue(Queue && other);        // remember: no const other object because it is being moved
+
+	Queue& operator=(const Queue &other);
+	Queue& operator=(Queue &&other);
+
 	~Queue() {}
+	
+	
 	void Enqueue(int value);
 	bool IsEmpty();
 	int Dequeue();
 
 };
+// move assignment 
+Queue& Queue::operator=(Queue &&other)
+{
+	std::cout << "move-assign the queue" << std::endl;
+	if (&other != this)
+	{
+		active  = std::move(other.active);
+		passive = std::move(other.passive);
+	}
+	return *this;
+}
 
+//copy assignment 
+Queue& Queue::operator=(const Queue &other)
+{
+	std::cout << "copy-assign the queue" << std::endl;
+	if (&other != this)     // to handle q1=q1
+	{
+		active.reset (new std::stack<int>(*other.active));
+		passive.reset (new std::stack<int>(*other.passive));
+	}
+	return *this;
+}
+
+//move constructor
+Queue::Queue(Queue && other): active(std::move(other.active)), passive(std::move(other.passive))
+{
+	std::cout << "moving the queue" << std::endl;
+}
+
+//copy constructor
+Queue::Queue(const Queue & other) : active (new std::stack<int>(*other.active)), passive (new std::stack<int>(*other.passive))
+{
+	std::cout << "copying the queue" << std::endl;
+}
 
 /* 
  * push elements on active stack for enqueue operation 
@@ -65,11 +107,19 @@ bool Queue::IsEmpty()
 
 int main()
 {
-	Queue q1;
-	// Queue q2(q1);   // compilation error due to deleted copy constructor
+	Queue q1;		//default 
+	Queue q2(q1);   	//copy
+	Queue q3 (std::move(q2)); // move constructor
+
+
+	Queue Q4;
+	Queue Q5; 
+	Queue Q6;
+
+	Q4 = Q5; 		//copy assignment
+	Q5 = std::move(Q6);	//move assignment
+
 	
-	Queue q2;
-	//q2 = q1;         // compilation error due to deleted = operator
 
 	q1.Enqueue(5);
 	q1.Enqueue(6);
